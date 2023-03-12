9c8011e8e844979c73607168a7126981b9a5f1fa
Ryan <ryanbiy01@gmail.com>
2023-03-12 12:26:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 30;
+test('height is less than 40', () => {
+    expect(height).toBeLessThan(40);
+  }); 
 const message = `${name} is ${height} inches tall`;
-
 module.exports = {name, height, message};
+console.log(message)
